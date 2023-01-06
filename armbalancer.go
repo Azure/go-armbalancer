1035a2db5ee680d85926c7a349e6ffbf5f683575
Jordan Olshevski <jordan.olshevski@microsoft.com>
2023-01-06 15:16:35 -0600

    Initial commit

---
+++ armbalancer.go
@@ -0,0 +1,137 @@
+package armbalancer
+
+import (
+	"math"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
+)
+
+const rateLimitHeaderPrefix = "X-Ms-Ratelimit-Remaining-"
+
+// New wraps a transport to provide smart connection pooling and client-side load balancing.
+// Assumes all requests using the returned transport will have the same host (ARM).
+//
+// Connections are recycled when recycleThreshold is <= the lowest value of any X-Ms-Ratelimit-Remaining-* header.
+//
+// minReqsBeforeRecycle is a safeguard to prevent frequent connection churn in the unlikely event
+// that a connections lands on an ARM instance that already has a depleted rate limiting quota.
+func New(transport *http.Transport, poolSize, recycleThreshold, minReqsBeforeRecycle int64) http.RoundTripper {
+	return newTransportPool(http.DefaultTransport.(*http.Transport), poolSize, recycleThreshold, minReqsBeforeRecycle)
+}
+
+type transportPool struct {
+	pool   []http.RoundTripper
+	cursor int64
+}
+
+func newTransportPool(parent *http.Transport, size, recycleThreshold, minReqsBeforeRecycle int64) *transportPool {
+	t := &transportPool{pool: make([]http.RoundTripper, size)}
+	for i := range t.pool {
+		t.pool[i] = newRecyclableTransport(i, parent, recycleThreshold, minReqsBeforeRecycle)
+	}
+	return t
+}
+
+func (t *transportPool) RoundTrip(req *http.Request) (*http.Response, error) {
+	i := int(atomic.AddInt64(&t.cursor, 1)) % len(t.pool)
+	return t.pool[i].RoundTrip(req)
+}
+
+type recyclableTransport struct {
+	lock        sync.Mutex // only hold while copying pointer - not calling RoundTrip
+	current     *http.Transport
+	counter     int64 // atomic
+	activeCount *sync.WaitGroup
+	state       *connState
+	signal      chan struct{}
+}
+
+func newRecyclableTransport(id int, parent *http.Transport, recycleThreshold, minReqsBeforeRecycle int64) *recyclableTransport {
+	r := &recyclableTransport{
+		current:     parent.Clone(),
+		activeCount: &sync.WaitGroup{},
+		state:       newConnState(),
+		signal:      make(chan struct{}, 1),
+	}
+	go func() {
+		for range r.signal {
+			if r.state.Min() > recycleThreshold || atomic.LoadInt64(&r.counter) < minReqsBeforeRecycle {
+				continue
+			}
+
+			// Swap a new transport in place while holding a pointer to the previous
+			r.lock.Lock()
+			previous := r.current
+			previousActiveCount := r.activeCount
+			r.current = parent.Clone()
+			atomic.StoreInt64(&r.counter, 0)
+			r.activeCount = &sync.WaitGroup{}
+			r.lock.Unlock()
+
+			// Wait for all active requests against the previous transport to complete before closing its idle connections
+			previousActiveCount.Wait()
+			previous.CloseIdleConnections()
+		}
+	}()
+	return r
+}
+
+func (t *recyclableTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	t.lock.Lock()
+	tx := t.current
+	wg := t.activeCount
+	t.lock.Unlock()
+
+	wg.Add(1)
+	resp, err := tx.RoundTrip(req)
+	wg.Add(-1)
+	atomic.AddInt64(&t.counter, 1)
+
+	if resp != nil {
+		t.state.ApplyHeader(resp.Header)
+	}
+
+	select {
+	case t.signal <- struct{}{}:
+	default:
+	}
+	return resp, err
+}
+
+type connState struct {
+	lock  sync.Mutex
+	types map[string]int64
+}
+
+func newConnState() *connState {
+	return &connState{types: make(map[string]int64)}
+}
+
+func (c *connState) ApplyHeader(h http.Header) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	for key, vals := range h {
+		if !strings.HasPrefix(key, "X-Ms-Ratelimit-Remaining-") {
+			continue
+		}
+		n, err := strconv.ParseInt(vals[0], 10, 0)
+		if err != nil {
+			continue
+		}
+		c.types[key[len(rateLimitHeaderPrefix):]] = n
+	}
+}
+
+func (c *connState) Min() int64 {
+	var min int64 = math.MaxInt64
+	for _, val := range c.types {
+		if val < min {
+			min = val
+		}
+	}
+	return min
+}
---
+++ example/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+
+	"github.com/Azure/armbalancer"
+)
+
+func main() {
+	cred, err := azidentity.NewAzureCLICredential(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	token, err := cred.GetToken(context.TODO(), policy.TokenRequestOptions{Scopes: []string{"https://management.core.windows.net"}})
+	if err != nil {
+		panic(err)
+	}
+
+	client := &http.Client{Transport: armbalancer.New(http.DefaultTransport.(*http.Transport), 4, 11997, 0)}
+
+	for {
+		do(client, token)
+		time.Sleep(time.Millisecond * 50)
+	}
+}
+
+func do(client *http.Client, token azcore.AccessToken) {
+	req, err := http.NewRequest("GET", os.Getenv("TEST_RESOURCE"), nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Authorization", "Bearer "+token.Token)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	log.Printf("response status %d with rate limit remaining %s", resp.StatusCode, resp.Header.Get("x-ms-ratelimit-remaining-subscription-reads"))
+}
